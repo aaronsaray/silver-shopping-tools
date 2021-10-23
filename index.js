0c296b84db48c9d96bf118906fb33e252c1f7ac6
Aaron Saray <me@aaronsaray.com>
2021-10-23 14:06:36 -0500

    updated for junk

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ function getApmexBuffalo1oz(data) {
     return priceHTML.text().replace('$', '').trim();
 }
 
+function getApmexJunk5Dimes(data) {
+    const $ = cheerio.load(data);
+
+    const priceHTML = $("[data-range='Any Quantity'] td:nth-child(2)");
+    return priceHTML.text().replace('$', '').trim();
+}
+
 function getJMASEPrice(data) {
     const $ = cheerio.load(data);
 
@@ -77,29 +84,40 @@ function getMMXBuffalo1oz(data) {
     return priceHTML.text().replace('$', '').trim();
 }
 
+function getMMXJunk5Dimes(data) {
+    const $ = cheerio.load(data);
+
+    const priceHTML = $("#premium_data_parent tr[data-range='$ 5 Face'] td:last-child");
+    return priceHTML.text().replace('$', '').trim();
+}
+
 axios.all([
     axios.get('https://api.metals.live/v1/spot'),
     axios.get('https://www.apmex.com/product/23331/1-oz-american-silver-eagle-coin-bu-random-year'),
     axios.get('https://www.apmex.com/product/21/10-oz-silver-bar-secondary-market'),
     axios.get('https://www.apmex.com/product/44447/1-oz-silver-round-buffalo'),
+    axios.get('https://www.apmex.com/product/213198/90-silver-coins-5-face-value-roll-dimes'),
     axios.get('https://www.jmbullion.com/american-silver-eagle-varied-year/'),
     axios.get('https://www.jmbullion.com/10-oz-silver-bar/'),
     axios.get('https://www.jmbullion.com/1-oz-sunshine-buffalo-silver-round/'),
     axios.get('https://www.moneymetals.com/silver-american-eagle-one-ounce-coin/9'),
     axios.get('https://www.moneymetals.com/10-oz-silver-bars/37'),
     axios.get('https://www.moneymetals.com/1-oz-silver-buffalo-round/158'),
+    axios.get('https://www.moneymetals.com/mercury-dimes/279'),
 ]).then(axios.spread(
     (
         spotPriceResponse,
         apmexASEResponse,
         apmex10ozResponse,
         apmex1ozBuffaloResponse,
+        apmexJunk5DimesResponse,
         jmASEResponse,
         jm10ozResponse,
         jm1ozBuffaloResponse,
         mmxASEResponse,
         mmx10ozResponse,
         mmx1ozBuffaloResponse,
+        mmxJunk5DimesResponse,
     ) => {
         return {
             spotPrice: {
@@ -122,6 +140,11 @@ axios.all([
                     price: getApmexBuffalo1oz(apmex1ozBuffaloResponse.data),
                     source: "https://www.apmex.com/product/44447/1-oz-silver-round-buffalo",
                     description: "apmex - 1oz silver round - buffalo - 1-19 check/wire"
+                },
+                junk5Dimes: {
+                    price: getApmexJunk5Dimes(apmexJunk5DimesResponse.data),
+                    source: "https://www.apmex.com/product/213198/90-silver-coins-5-face-value-roll-dimes",
+                    description: "apmex - $5 dimes junk - any"
                 }
             },
             jm: {
@@ -140,6 +163,11 @@ axios.all([
                     source: "https://www.jmbullion.com/1-oz-sunshine-buffalo-silver-round/",
                     description: "jm bullion - 1oz silver round buffalo (SMI)  - 1-19 check/wire"
                 },
+                junk5Dimes: {
+                    price: '',
+                    source: '',
+                    description: 'none',
+                }
             },
             mmx: {
                 ASE: {
@@ -156,6 +184,11 @@ axios.all([
                     price: getMMXBuffalo1oz(mmx1ozBuffaloResponse.data),
                     source: "https://www.moneymetals.com/1-oz-silver-buffalo-round/158",
                     description: "mmx - 1oz silver round buffalo - 1-39"
+                },
+                junk5Dimes: {
+                    price: getMMXJunk5Dimes(mmxJunk5DimesResponse.data),
+                    source: "https://www.moneymetals.com/mercury-dimes/279",
+                    description: "mmx - $5 dimes junk mercury"
                 }
             },
             timestamp: Math.floor(new Date().getTime() / 1000)
